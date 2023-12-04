86a0b44af1b013c3cfac6d657d850cc7e272cbd0
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2023-12-04 02:02:31 +0000

    done

--- 02_Array/01_declar.js
+++ 02_Array/01_declar.js
@@ -3,3 +3,11 @@ console.log(a);
 
 let b = new array("abc","def","ghi")
 console.log(b);
+
+/**
+ * 
+ * given array --->[1,2,3,4,5]
+ * index/pos ------>[0,1,2,3,4] // last element index = arr.lenth-1
+ */
+
+console.log(a[4]);
---
+++ 02_Array/02_arrayMethod.js
@@ -0,0 +1,18 @@
+let arr = [20,34,42,56]
+arr.push(78,65);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+let f=arr.shift();
+console.log(f);
+console.log(arr);
+
+arr.unshift(3)
+console.log(arr);
+
+let a1 = [3,4,6,3]
+let a2 = [6,4,2,1,]
+let a3 = a1.concat(a2);
+console.log(a1,a2,a3)
