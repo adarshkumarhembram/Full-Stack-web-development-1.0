fefe1d771e7dee3cae79558d74da10e0d9913aeb
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2023-12-09 12:06:08 +0000

    switch condition

---
+++ 05_conditions/05_SwitchAndcases.js
@@ -0,0 +1,23 @@
+let month = 4;
+
+switch (month) {
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+            console.log("Tuesday");
+            break;
+            case 3:
+    console.log("wednesday");
+        break;
+        case 4:
+    console.log("thrusday");
+        break;    
+        case 5:
+    console.log("friday");
+        break;
+
+    default:
+        console.log("holiday");
+        break;
+}
