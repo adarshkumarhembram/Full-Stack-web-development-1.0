4b79f5e87472e54812bce00e2c1c89a1fc9c59a1
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2023-12-10 02:16:29 +0000

    condition

---
+++ 05_conditions/06_rivision.js
@@ -0,0 +1,13 @@
+let star = 5;
+if (star == 5) {
+    console.log("Execellent");
+    
+}else if (star == 4){
+    console.log("good service");
+}else if (star==3){
+    console.log("ok service");
+}else if (star==2){
+    console.log("need to improve");
+}else {
+    console.log("not right");
+}
---
+++ 05_conditions/07_rivision.js
@@ -0,0 +1,45 @@
+// let UserName = "abcd";
+// let PassWord = 1234;
+
+// if(UserName=="abcd" && PassWord==1234){
+//     console.log("you are able to login");
+// }else{
+//     console.log("not able to log in");
+// }
+
+
+// let rating=2;
+
+// if(rating=5){
+//     console.log("rating 5");
+// }else if (rating=4){
+//     console.log("rating 4");
+// }else if(rating=3){
+//     console.log("rating 3");
+// }else if (rating = 2){
+//     console.log("rating 2");
+// }else if (rating =1){
+//     console.log("rating 1");
+// }
+
+// let user = 'admin';
+// switch (user) {
+//     case 'Mentor':
+//         console.log('redirected to the user page');
+        
+//         break;
+//     case 'admin':
+//     console.log('redirected to the admin page');
+    
+//     default:
+//         console.log('no goes anywhere');
+        
+// }
+
+let Chitranjanfees=1700;
+
+if(Chitranjanfees==1700){
+    console.log("you are able to join");
+}else {
+    console.log("you are not able to join");
+}
