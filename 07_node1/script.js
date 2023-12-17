0931b76183669a7d5f8e4d38eecce23aae9818d6
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2023-12-17 05:11:24 +0000

    node

---
+++ 07_node1/script.js
@@ -0,0 +1,20 @@
+
+
+// // // let n =5;
+
+// // // for (let i=0; i<n; i++){
+// // //     console.log("hello baccho");
+// // // }
+// // // console.log("bye");
+
+// // console.log(process.argv);
+
+// let args=process.argv;
+
+// for(let i=2; i<args.length; i++ ){
+//     console.log("hello & welcome ",args[i]);
+// }
+
+const math = require("./math");
+console.log(math);
+
